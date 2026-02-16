de6e7d55343eb7a75b303373181fec11d7f6b07b
its-huzi-18 <157364561+its-huzi-18@users.noreply.github.com>
2026-02-16 23:14:21 +0500

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "./src");
+    config.resolve.alias["@/lib"] = path.resolve(__dirname, "./src/lib");
+    config.resolve.alias["@/components"] = path.resolve(__dirname, "./src/components");
+    config.resolve.alias["@/hooks"] = path.resolve(__dirname, "./hooks");
+    config.resolve.alias["@/app"] = path.resolve(__dirname, "./app");
+    return config;
+  },
 };
 
 export default nextConfig;
